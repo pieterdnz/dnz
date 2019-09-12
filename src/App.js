8004bac59fd909845549839c90b3b4f3301eed14
pieter bos <pieter@denieuwezaak.nl>
2019-09-12 14:31:09 +0200

    ads

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './download.gif';
 import './App.css';
-import {isAndroid,isMobile,isIOS} from 'react-device-detect';
+import {isAndroid,isMobile,isIOS,isChrome} from 'react-device-detect';
 
 function App() {
   return (
@@ -11,13 +11,16 @@ function App() {
         <p>
           Maqro on {isAndroid && 'Android' }  {isMobile && 'Mobile' }  {isIOS && 'Mobile' } 
         </p>
+        <p>
+          {isChrome && 'Chorme'}
+        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.makrokerstpakketten.nl/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Bij makrokerstpakketten shop je voor valentijn gifts
         </a>
       </header>
     </div>
