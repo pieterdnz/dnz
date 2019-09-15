c91439a0a1f2091d58cf1271a0b7cae492045aad
pieter bos <pieter@denieuwezaak.nl>
2019-09-15 13:05:02 +0200

    converted to dnz

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './download.gif';
-import makro from './makro-logo.svg';
+//import logo from './download.gif';
+//import makro from './makro-logo.svg';
 import './App.css';
-import {isAndroid,isMobile,isIOS,isChrome} from 'react-device-detect';
+import {isAndroid,isMobile,isIOS,isChrome, isIE,BrowserView} from 'react-device-detect';
 
 function App() {
   setTimeout(function(){
-    window.location.href = 'https://www.makrokerstpakketten.nl/geven-wordt-een-feestje/page/830/';
+    window.location.href = 'https://www.denieuwezaak.nl';
  }, 7000);
 
   return (
     <div className="App" >
-      <div  className="makro"><img alt="makro" src={makro}/></div>
+      {/* <div  className="makro"><img alt="makro" src={makro}/></div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -30,9 +30,18 @@ function App() {
         </a>
 
        
-      </header>
+      </header> */}
+      <h1>DNZ ❤ {isAndroid && 'Android' }  {isMobile && 'Mobile' }  {isIOS && 'IOS' } {isChrome && 'Chrome'} {isIE && 'IE'}  </h1>
+      <BrowserView>
+      <p>
+      Normal browser
+      </p>
+      
+      </BrowserView>
     </div>
   );
 }
+
+
 
 export default App;
